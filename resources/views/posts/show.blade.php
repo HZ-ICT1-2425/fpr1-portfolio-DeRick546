490a1ab8c3df966a76cb7e651e91e17eef802205
Rick <cuij0003@hz.nl>
2025-02-18 09:57:23 +0100

    bug_within_running_the_code

--- resources/views/posts/show.blade.php
+++
@@ -1,9 +0,0 @@
-<x-layout.main>
-    <h1 class="title is-4">{{ $post->title }}</h1>
-    <h2 class="subtitle is-6 is-italic">
-        {!! $post->excerpt !!}
-    </h2>
-    <div>
-        {!! $post->body !!}
-    </div>
-</x-layout.main>
